125ff41995e394502acee7806368368b2b080710
TayeYanis <yanis.taye.2001@gmail.com>
2024-09-30 18:44:45 +0200

    feat: fixtures

---
+++ wr506d/migrations/Version20240930122439.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240930122439 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE actor ADD death_date DATE DEFAULT NULL, CHANGE updated_at updated_at DATETIME NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE actor DROP death_date, CHANGE updated_at updated_at DATETIME DEFAULT NULL');
+    }
+}
